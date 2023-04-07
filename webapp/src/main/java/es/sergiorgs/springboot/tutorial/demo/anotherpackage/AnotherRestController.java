a6dd936134f15c92a85368d1e5e8ed884f9edc71
Sergio R.Guasch <sergio.rodriguez@streamsets.com>
2023-04-07 10:45:49 -0700

    Learned about primary annotation

--- webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/anotherpackage/AnotherRestController.java
+++ webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/anotherpackage/AnotherRestController.java
@@ -1,12 +1,24 @@
 package es.sergiorgs.springboot.tutorial.demo.anotherpackage;
 
+import es.sergiorgs.springboot.tutorial.demo.webapp.bean.Animal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnotherRestController {
+
+    private Animal animal;
+
     @GetMapping("/another")
     public String another() {
-        return "Hey, you found me!";
+        return animal.getSound();
+    }
+
+    // This constructor should receive a dog
+    // since it's the @Primary impl
+    @Autowired
+    public AnotherRestController(Animal animal) {
+        this.animal = animal;
     }
 }
--- webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/webapp/bean/Dog.java
+++ webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/webapp/bean/Dog.java
@@ -1,8 +1,10 @@
 package es.sergiorgs.springboot.tutorial.demo.webapp.bean;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Dog implements Animal {
     @Override
     public String getSound() {
