0a53f4829db4c98bebc043be77395a82018314e1
Sergio R.Guasch <sergio.rodriguez@streamsets.com>
2023-04-07 08:23:46 -0700

    Testing app.properties

--- webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/webapp/rest/FunRestController.java
+++ webapp/src/main/java/es/sergiorgs/springboot/tutorial/demo/webapp/rest/FunRestController.java
@@ -1,12 +1,17 @@
 package es.sergiorgs.springboot.tutorial.demo.webapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${cat.name}")
+    private String catName;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return catName;
     }
 }
