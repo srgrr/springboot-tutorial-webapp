b69b29cc67fca8696cb3032f7d687702f3742fa7
Sergio R.Guasch <sergio.rodriguez@streamsets.com>
2023-04-09 11:50:14 -0700

    Add table creation script

--- webapp/dev/create_db.py
+++ webapp/dev/create_db.py
@@ -1,33 +1,41 @@
 import argparse
-import mysql
+import mysql.connector
 
 
 class Connection(object):
-  def __init__(db_url, db_user, db_pass):
+  def __init__(self, db_url, db_port, db_user, db_pass):
     self.db_url = db_url
+    self.db_port = db_port
     self.db_user = db_user
     self.db_pass = db_pass
 
-  @property
-  def conn(self):
-    pass
-
-  def run_trx(self, query):
-    pass
+  def get_connection(self):
+    return mysql.connector.connect(
+      host=self.db_url,
+      port=self.db_port,
+      user=self.db_user,
+      password=self.db_pass
+    )
 
 
 def parse_args():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--db-url")
-  parser.add_argument("--db-user")
-  parser.add_argument("--db-pass")
+  parser.add_argument("--db-url", type=str)
+  parser.add_argument("--db-port", type=int)
+  parser.add_argument("--db-user", type=str)
+  parser.add_argument("--db-pass", type=str)
   return parser.parse_args()
 
 
-def main(db_url, db_user, db_pass):
-  conn = Connection(db_url, db_user, db_pass)
-  conn.run_trx("CREATE TABLE 1")
+def main(db_url, db_port, db_user, db_pass):
+  conn = \
+    Connection(db_url, db_port, db_user, db_pass).get_connection()
+  cursor = conn.cursor()
+  cursor.execute("CREATE DATABASE IF NOT EXISTS example")
+  cursor.execute("USE example")
+  cursor.execute("CREATE TABLE IF NOT EXISTS PERSON (ID_CARD varchar(256), FULL_NAME varchar(256))")
 
 
 if __name__ == "__main__":
+  opts = parse_args()
   main(**vars(opts))
