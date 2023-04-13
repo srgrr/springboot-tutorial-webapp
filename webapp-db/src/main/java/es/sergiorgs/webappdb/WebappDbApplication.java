8e167837bc2e2ecc5942b34a63bfc3fa9823c883
Sergio R.Guasch <sergio.rodriguez@streamsets.com>
2023-04-13 10:23:02 -0700

    Add skeleton for webapp db with persistence

---
+++ webapp-db/src/main/java/es/sergiorgs/webappdb/WebappDbApplication.java
@@ -0,0 +1,13 @@
+package es.sergiorgs.webappdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappDbApplication.class, args);
+	}
+
+}
